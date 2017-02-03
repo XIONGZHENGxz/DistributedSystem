0bc79dc697ed000b170fb5cc680954574bbf424a
xiong <zhengxionguta@gmail.com>
2017-02-03 12:07:17 -0800

    ping monitor

--- lab2/viewservice/Server.java
+++ lab2/viewservice/Server.java
@@ -1,3 +1,63 @@
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
 public class Server{
-	
+	String host;//server host:port
+	String viewServer;//view service server host
+	int viewServerPort;//view service server port
+	int viewNum;
+	long lastPingTime;
+	public Server(String name,String server,int port){
+		this.host=name;
+		this.viewServer=server;
+		this.viewServerPort=port;
+		this.viewNum=0;
+		lastPingTime=System.currentTimeMillis();
+	}
+		
+	public Object Call(String msg,Object args){
+		try{
+			Registry registry=LocateRegistry.getRegistry(this.viewServer,this.viewServerPort);
+			ViewService stub=(ViewService) registry.lookup("view service");
+			if(msg.equals("ping")) return stub.Ping((PingArg)args);
+			else if(msg.equals("get")) return stub.Get();
+			else System.err.println("parameter error");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public PingReply Ping(int viewNum){
+		PingArg arg=new PingArg(viewNum++,this.host);
+		return (PingReply)this.Call("ping",arg);
+	}
+
+	public View Get(){
+		return (View)this.Call("get",null);
+	}
+
+	public String Primary(){
+		View view=this.Get();
+		return view.primary;
+	}
+
+	public static void main(String...args){
+		if(args.length<1){
+			System.err.println("args...host,view server host,view server port");
+			return;
+		}
+		Server server=new Server(args[0],args[1],Integer.parseInt(args[2]));
+		while(true){
+			long currentTime=System.currentTimeMillis();
+			if(currentTime-server.lastPingTime>=Common.PingInterval){
+				server.lastPingTime=currentTime;
+				server.Ping(server.viewNum);
+			}
+		}
+	}
+
 }
+
+
+
+
--- lab2/viewservice/ViewServer.java
+++ lab2/viewservice/ViewServer.java
@@ -1,3 +1,7 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
@@ -5,39 +9,81 @@ public class ViewServer implements ViewService{
 	String host;
 	int port;
 	View view;
-	public ViewServer(){}
+	Set<String> servers;
+	Map<String,Long> lastPingTime;
 	public ViewServer(String host,int port){
 		this.port=port;
 		this.host=host;
-		try{
-			ViewServer vs=new ViewServer();
-			System.setProperty("java.rmi.server.hostname","192.168.245.146");
-			ViewService stub=(ViewService) UnicastRemoteObject.exportObject(vs,0);
-			Registry registry=LocateRegistry.createRegistry(this.port);
-			registry.rebind("view service",stub);
-			System.out.println("view service is runing");
-		} catch(Exception e){
-			e.printStackTrace();
-		}
+		lastPingTime=new HashMap<>();
+		servers=new HashSet<>();
+		lastPingTime=new HashMap<>();
 	}
 
 	public PingReply Ping(PingArg args){
 		int viewNum=args.viewNum;
 		String hostPort=args.hostPort;
+		servers.add(hostPort);
+		long pingTime=System.currentTimeMillis();
+		lastPingTime.put(hostPort,pingTime);
 		if(view==null){
-			view=new View(1,hostPort,"");
+			view=new View(viewNum,hostPort,"");
 			System.out.println("assign primary to "+hostPort);
 		}
-		if(viewNum==0) System.out.println(hostPort+"crashed and restarted!");
+		if(viewNum==0) 
+			System.out.println(hostPort+"started!");
 		PingReply pr=new PingReply(this.view,false);
 		return pr;
 	}
-
+	
 	public View Get(){
 		return view;	
 	}
 
-	public static void main(String...args){
-		ViewServer vs=new ViewServer(args[0],Integer.parseInt(args[1]));
+	// server dead
+	public void DeclareDead(String server){
+		this.servers.remove(server);
 	}
+
+	//primary is dead, assign new primary 
+	public void AssignNewPrimary(){
+		if(this.servers.size()==0){
+			System.err.println("all servers are dead");
+			this.view=null;
+			return;
+		}
+		String prim=this.servers.iterator().next();
+		this.view.primary=prim;
+		this.view.backup="";
+	}
+
+	public static void main(String...args){
+		if(args.length<1){
+			System.out.println("args...host,port");
+			return;
+		}
+		ViewServer vs=new ViewServer(args[0],Integer.parseInt(args[1]));
+		try{
+			System.setProperty("java.rmi.server.hostname","192.168.245.146");
+			ViewService stub=(ViewService) UnicastRemoteObject.exportObject(vs,0);
+			Registry registry=LocateRegistry.createRegistry(vs.port);
+			registry.rebind("view service",stub);
+			System.out.println("view service is runing");
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		while(true){
+			for(String server:vs.lastPingTime.keySet()){
+				long timeNow=System.currentTimeMillis();
+				if((timeNow-vs.lastPingTime.get(server))>Common.DeadPings*Common.PingInterval){
+					System.out.println(server+"is dead!");
+					vs.DeclareDead(server);
+					if(vs.view.primary.equals(server)) 
+						vs.AssignNewPrimary();
+					else 
+						vs.view.backup="";
+				}
+			}
+		}
+	}
+
 }
