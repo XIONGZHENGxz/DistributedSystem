f94270384a202c61179f3c1fddf5b558ae3f577d
xiong <zhengxionguta@gmail.com>
2017-02-03 20:08:12 -0800

    serializable

--- lab2/pbviewservice/GetReply.java
+++ lab2/pbviewservice/GetReply.java
@@ -1,4 +1,6 @@
-public class GetReply<T>{
+import java.io.Serializable;
+public class GetReply<T> implements Serializable{
+	private final static long serialVersionUID=11L;
 	T value;
 	public GetReply(T val){
 		value=val;
--- lab2/pbviewservice/PutAppendReply.java
+++ lab2/pbviewservice/PutAppendReply.java
@@ -1,4 +1,6 @@
-public class PutAppendReply{
+import java.io.Serializable;
+public class PutAppendReply implements Serializable{
+	private final static long serialVersionUID=10L;
 	boolean err;
 	public PutAppendReply(boolean b){
 		err=b;
