e7aa03a95774113f1cd05be6fc857c4fe993c342
xiong <zhengxionguta@gmail.com>
2017-06-10 22:41:51 -0700

    new paxos test cases

--- PaxosBasedKVStore/src/test/java/uta/shan/paxostest/TestNewPaxos.java
+++ PaxosBasedKVStore/src/test/java/uta/shan/paxostest/TestNewPaxos.java
@@ -1,63 +1,64 @@
 package uta.shan.paxostest;
 
-import uta.shan.paxos.*;
+import uta.shan.paxos2.*;
 import org.junit.Test;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.*;
-public class TestPaxos{
-	//test single paxos
-	private static final String host="192.168.245.150";
+
+public class TestNewPaxos {
+	//test single paxosos
+	private static final String LOCALHOST="localhost";
+	private static final int NUMTHREADS = 10;
 	private int num;
-	String[] peers;
-	Paxos[] pax;
-	int[] ports;
-	ExecutorService es = Executors.newFixedThreadPool(10);
-	//init paxos
+	private String[] peers;
+	private Paxos<String>[] paxos;
+	private int[] ports;
+
+	private ExecutorService es = Executors.newFixedThreadPool(NUMTHREADS);
+
+	//init paxosos
 	public void initPaxos(int num){
 		this.num=num;
 		peers=new String[num];
 		ports=new int[num];
-		pax=new Paxos[num];
+		paxos=new Paxos[num];
 		for(int i=0;i<num;i++){
 			ports[i]=1099+i;
-			peers[i]=host;
+			peers[i]=LOCALHOST;
 		}
 		for(int i=0;i<num;i++){
-			pax[i]=new Paxos(i,this.peers,this.ports);
+			paxos[i]=new Paxos<>(this.peers,this.ports,i);
 		}
 	}
-		
+
 	public  int numOfDecided(int totalNum,int seq){
 		Object value=new Object();
 		Status s=null;
 		int count=0;
 		for(int i=0;i<totalNum;i++){
-			if(pax!=null){
-				Instance inst=pax[i].getMap().get(seq);
-				if(inst!=null){
-					s=inst.getStatus();
-					Object dVal=inst.getProposal().getValue();
-					if(s==Status.DECIDED){
-						if(count>0 && !value.equals(dVal))
-							return -1;	
-						count++;
-						value=dVal;
-					}
+			Instance inst = (Instance) paxos[i].getLearner().getSeqMap().get(seq);
+			if(inst != null){
+				s = inst.getStatus();
+				Object dVal=inst.getProposal().getValue();
+				if(s == Status.DECIDED){
+					if(count>0 && !value.equals(dVal)) return -1;
+					count++;
+					value = dVal;
 				}
 			}
 		}
 		return count;
-	}		
+	}
 
 	//wait paxos to decide
 	public boolean waitDecide(int totalNum,int seq){
 		int waitInterval=1000;
-		int iter=30;
-		int num=-1;
+		int iter = 30;
+		int num = -1;
 		while(iter-->0){
-			num=numOfDecided(totalNum,seq);
+			num = numOfDecided(totalNum,seq);
 			if(num>=totalNum/2+1) return true;
 			try{
 				Thread.sleep(waitInterval);
@@ -67,33 +68,25 @@ public class TestPaxos{
 		}
 		return false;
 	}
-	
-	//clean up
-	public void cleanup(){
-		for(int i=0;i<this.num;i++){
-			this.pax[i].cleanup();
-		}
-	}
 
 	//test single paxos server
-	@Test 
+	@Test
 	public void test1(){
 		int n=1;
-		this.initPaxos(n);
-		pax[0].start(0,"hello");
-		pax[0].run();
+		initPaxos(n);
+		paxos[0].startConcensus(0,"hello");
+		paxos[0].run();
 		assertTrue(waitDecide(n,0));
-		this.cleanup();
 	}
 
 	//multiple proposer
 	@Test
 	public void test2(){
 		int n=2;
-		this.initPaxos(n);
-		pax[0].start(0,"hello");
-		pax[1].start(0,"lang");
-		for(int i=0;i<n;i++) es.execute(pax[i]);
+		initPaxos(n);
+		paxos[0].startConcensus(0,"hello");
+		paxos[1].startConcensus(0,"lang");
+		for(int i=0;i<n;i++) es.execute(paxos[i]);
 		boolean ok=false;
 		try{
 			es.shutdown();
@@ -103,19 +96,18 @@ public class TestPaxos{
 		}
 		assertTrue(ok);
 		assertTrue(waitDecide(n,0));
-		this.cleanup();
 	}
-	
+
 	@Test
 	public void test3(){
 		int n=5;
 		this.initPaxos(n);
-		pax[0].start(0,"hello");
-		pax[1].start(0,"lang");
-		pax[2].start(0,"zheng");
-		pax[3].start(1,"xiong");
-		pax[4].start(1,"xz");
-		for(int i=0;i<n;i++) es.execute(pax[i]);
+		paxos[0].startConcensus(0,"hello");
+		paxos[1].startConcensus(0,"lang");
+		paxos[2].startConcensus(0,"zheng");
+		paxos[3].startConcensus(1,"xiong");
+		paxos[4].startConcensus(1,"xz");
+		for(int i=0;i<n;i++) es.execute(paxos[i]);
 		boolean ok=false;
 		try{
 			es.shutdown();
@@ -125,6 +117,5 @@ public class TestPaxos{
 		}
 		assertTrue(ok);
 		assertTrue(waitDecide(n,0));
-		this.cleanup();
-	}		
+	}
 }
