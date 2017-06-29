961fbdded81844a91323fcc2b1fe082bcaceead2
xiong <zhengxionguta@gmail.com>
2017-06-28 20:00:00 -0700

    test

---
+++ Util.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * Created by xz on 6/7/17.
+ */
+public class Util {
+    public static final int TIMEOUT = 1000;
+    public static final int PAXOS_TIMEOUT = 3000;
+    public static final int paxosPort = 9999;
+    public static final int clientPort = 8888;
+    public static final boolean DEBUG = true;
+    public static long getCurrTime() {
+        return System.currentTimeMillis();
+    }
+}
